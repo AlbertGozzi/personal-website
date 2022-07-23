b2e792cd3225f84a8f5fc12a1b9781e87f7f1675
Albert Gozzi <albertgozzi@gmail.com>
2022-07-23 16:59:34 -0400

    finished header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Header from "./components/Header.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <div class="grid grid-cols-5 gap-3">
+        <div class="bg-blue-100">1st col</div>
+        <div class="bg-red-100 col-span-4">2nd col</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faAngellist,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Header = () => {
+  return (
+    <div class="flex justify-between content-center my-8 py-1 border-gray-50 border-b-2">
+      <div class="font-extrabold">Albert Gozzi</div>
+      <div class="flex space-x-3 content-center">
+        <a href="https://github.com/albertgozzi">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://www.facebook.com/albert.gozzi/">
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a href="https://instagram.com/albertgozzi">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://www.linkedin.com/in/albertgozzi">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href="https://angel.co/p/albert-gozzi">
+          <FontAwesomeIcon icon={faAngellist} />
+        </a>
+        <a href="https://twitter.com/albertgozzi">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
